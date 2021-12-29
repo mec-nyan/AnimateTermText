0d9263921ae401b465dd1b1a6abd1d9a918af0b9
mec-nyan <marianoraspberrypi@gmail.com>
2021-12-29 18:01:00 -0300

    Incorporate parsing (syntax) and animation into main editor window

--- Python/curses/editor_animated.py
+++ Python/curses/editor_animated.py
@@ -2,6 +2,7 @@
 '''Get terminal size (columns and lines) and split in rects (windows)'''
 
 import curses
+from js_parser import parse
 
 
 def main(screen):
@@ -76,7 +77,7 @@ def main(screen):
     title = curses.color_pair(18)
 
     # Editor windows
-    editor = curses.newwin(lines -1, columns - 6, 0, 6)
+    editor = curses.newwin(lines -2, columns - 6, 0, 6)
     curses.init_pair(2, editor_fg, editor_bg)
     editor_color = curses.color_pair(2)
     editor.bkgdset(editor_color)
@@ -195,6 +196,62 @@ def main(screen):
     statusline.refresh()
 
     screen.getch()
+
+    with open('helloWorld.js') as f:
+        code = f.read()
+
+    editor.clear()
+    editor.addstr(code)
+    editor.getch()
+
+    editor.clear()
+
+    tokens = parse(code)
+    
+    colours = []
+    for i in range(16):
+        curses.init_pair(i + 20, i, editor_bg)
+        colours.append(curses.color_pair(i + 20))
+
+
+    while tokens:
+        t = tokens.pop(0)
+
+        if t.name == 'bracket':
+            editor.bkgdset(colours[7])
+        elif t.name == 'op':
+            editor.bkgdset(colours[5])
+        elif t.name == 'num':
+            editor.bkgdset(colours[11])
+        elif t.name == 'string':
+            editor.bkgdset(colours[3])
+        elif t.name == 'comment':
+            editor.bkgdset(colours[0])
+        elif t.name == 'mlc':
+            editor.bkgdset(colours[14])
+        elif t.name == 'word':
+            if t.type_ == 'kw':
+                editor.bkgdset(colours[5])
+            elif t.type_ == 'bool':
+                editor.bkgdset(colours[6])
+            elif t.type_ == 'ret':
+                editor.bkgdset(colours[1])
+            elif t.type_ == 'func':
+                editor.bkgdset(colours[2])
+            else:
+                editor.bkgdset(colours[-1])
+        elif t.name == 'generic' or t.name == 'wsp':
+            editor.bkgdset(colours[-1])
+
+        fps = 20
+        for c in t.chars:
+            editor.addstr(c)
+            editor.refresh()
+            curses.napms(1000 // fps)
+
+    editor.getch()
+
+
 
 
 if __name__ == '__main__':
