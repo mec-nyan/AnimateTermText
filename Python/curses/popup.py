ecbfb84bcd8d1938e122d7f1d160ab0290838cf7
mec-nyan <marianoraspberrypi@gmail.com>
2021-12-30 00:12:36 -0300

    trying popup ideas, with colours and animation

--- Python/curses/popup.py
+++ Python/curses/popup.py
@@ -14,23 +14,41 @@ def main(screen):
     for i in range(16):
         curses.init_pair(i + 1, i, -1)
         colors.append(curses.color_pair(i + 1))
-
+    colors.append(curses.color_pair(0)) # default fg bg
 
     lines, columns = screen.getmaxyx()
 
-    topw = curses.newwin(lines, columns)
-    topw.bkgdset(colors[1])
-    topw.clrtobot()
-    topw.box()
+    screen.bkgdset(colors[4])
+    title = ' Popup windows '
+    screen.box()
+    screen.addstr(0, (columns - len(title)) // 2, title)
+    screen.noutrefresh()
+
+    topw_h = lines -4
+    topw_w = columns - 4
+    topw_y = 2 
+    topw_x = 2
+    topw = curses.newwin(topw_h, topw_w, topw_y, topw_x)
+    topw.bkgdset(colors[-1])
+    topw.addstr('Trying out several popup ideas in python with curses.')
+    topw.addstr(topw_h - 1, 0, 'Press any key...')
     topw.getch()
+
+    # Popup: vertical animation
+    topw.move(0, 0)
+    topw.clrtoeol()
+    topw.addstr(0, 0, 'Animated popup border window')
+    topw.noutrefresh()
 
     popup_width = 30
     popup_height = 20
+    popup_y = 4
+    popup_x = 4
     popup_title = ' <tip/> '
     padding_left = (popup_width - len(popup_title)) // 2
     fps = 40
 
-    popup = curses.newwin(1, popup_width, 4, 4)
+    popup = curses.newwin(1, popup_width, popup_y, popup_x)
     popup.bkgdset(colors[2])
     for i in range(popup_height):
         popup.clear()
@@ -42,23 +60,95 @@ def main(screen):
 
     popup.getch()
 
-    topw.clear()
-    topw.box()
-    topw.refresh()
-    popup.mvwin(4, 8)
+    while popup_x < 40:
+        topw.clear()
+        topw.addstr(0, 0, 'Move the popup window')
+        topw.addstr(topw_h - 1, 0, 'Press any key...')
+        topw.refresh()
+        popup.mvwin(popup_y, popup_x)
+        popup.refresh()
+        popup_x += 1
+        curses.napms(1000 // fps)
     popup.getch()
 
     # Post it-like notes 
-    curses.init_pair(17, 239, 3)
-    yellow_bg = curses.color_pair(17)
+    topw.clear()
+    topw.addstr('Post-it-like notes')
+    topw.addstr(topw_h - 1, 0, 'Press any key...')
+    topw.noutrefresh()
 
-    postit = curses.newwin(10, 20, 2, 50)
-    postit.bkgdset(yellow_bg)
+    colored_bg = []
+    start = 17
+    for i in range(16):
+        curses.init_pair(start, 234, i)
+        colored_bg.append(curses.color_pair(start))
+        start += 1
+
+    postit_h = 10 
+    postit_w = 30
+    postit_y = (topw_h - postit_h) // 2
+    postit_x = (topw_w - postit_w) // 2
+    postit = curses.newwin(postit_h, postit_w, postit_y, postit_x)
+    postit.bkgdset(colored_bg[4])
     postit.clear()
-    postit.addstr(1, 1, "I'm a note...", curses.A_ITALIC)
+    postit.attron(curses.A_ITALIC)
+    note = [
+        "I'm a note!",
+        "",
+        "And this is a list:",
+        "  ◽ One thing for sure",
+        "  ◽ you should've known",
+        "  ◽ is that, in the end",
+    ]
+    for i in range(len(note)):
+        postit.addstr(1 + i, 1, note[i])
     postit.refresh()
 
+    postit.getch()
+
+    topw.clear()
+    topw.addstr('Post-it-like notes')
+    topw.addstr(topw_h - 1, 0, 'Press any key...')
+    topw.noutrefresh()
+
+    # Animate with mask
+    topw.move(0, 0)
+    topw.clear()
+    topw.addstr('Vertical animation')
+    topw.addstr(topw_h - 1, 0, 'Press any key...')
     topw.getch()
+
+    mask_h = 0
+    mask_w = postit_w
+    mask = postit.derwin(mask_h, mask_w, 0, 0)
+
+    fps = 25
+    while mask_h < postit_h:
+        mask_h += 1
+        mask.resize(mask_h, mask_w)
+        mask.refresh()
+        curses.napms(1000 // fps)
+
+    mask.getch()
+
+    topw.move(0, 0)
+    topw.clear()
+    topw.addstr('Horizontal animation')
+    topw.addstr(topw_h - 1, 0, 'Press any key...')
+    topw.getch()
+
+    mask_h = postit_h
+    mask_w = 0
+
+    fps = 60
+    while mask_w < postit_w:
+        mask_w += 1
+        mask.resize(mask_h, mask_w)
+        mask.refresh()
+        curses.napms(1000 // fps)
+
+    mask.getch()
+    
 
 
 if __name__ == '__main__':
