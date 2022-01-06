ded14f0279f2012b9361d94736622ade612f414b
mec-nyan <marianoraspberrypi@gmail.com>
2022-01-05 21:13:39 -0300

    pause

--- Python/curses/helloWorld.js
+++ Python/curses/helloWorld.js
@@ -7,12 +7,14 @@
 // declare a variable
 // pause
 let x = 0.24;
+// pause
 const PI = 3.1416;
 // pause
 
 function put(text) {
 // pause
   console.log(text);
+// pause
   return true;
 }
 // pause
@@ -20,9 +22,11 @@ function put(text) {
 for (let i = 0; i < 10; ++i) {
 // pause
   if (i % 2 === 0) {
+// pause
     put('"i" is even');
 // pause
   } else {
+// pause
     put('"i" is odd');
   }
 }
