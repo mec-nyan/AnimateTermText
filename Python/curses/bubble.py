5d6e34fc7a29fd577a9caaa803964bc6301cb299
mec-nyan <marianoraspberrypi@gmail.com>
2022-01-17 21:13:20 -0300

    Classic bubble!

--- Python/curses/bubble.py
+++ Python/curses/bubble.py
@@ -10,6 +10,15 @@ def main(screen):
     curses.start_color()
     curses.use_default_colors()
 
+    tl_arrow = ''
+    tr_arrow = ''
+    bl_arrow = ''
+    br_arrow = ''
+    l_arrow = ''
+    r_arrow = ''
+    l_empty_arrow = ''
+    r_empty_arrow = ''
+
     colors = []
     for i in range(16):
         curses.init_pair(i + 1, i, -1)
@@ -25,6 +34,20 @@ def main(screen):
     win.box()
     win.addstr(0, padding_left, title)
     win.getch()
+
+    bubble = curses.newwin(5, 20, 8, 18)
+    bubble.bkgdset(colors[4])
+    bubble.addstr(2, 0, l_arrow)
+    bubble.refresh()
+    inner_bubble = curses.newwin(5, 19, 8, 19)
+    inner_bubble.bkgdset(colors[4] | curses.A_REVERSE)
+    inner_bubble.clrtobot()
+    inner_bubble.addstr(1, 1, 'Hey!')
+    inner_bubble.addstr(3, 1, "I'm a bubble!")
+    inner_bubble.refresh()
+
+    win.getch()
+
 
 
 if __name__ == '__main__':
